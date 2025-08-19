060af7395f35b7e1b56d8675cb2741543e083799
reven0n <darientan.ssis@gmail.com>
2025-08-19 15:18:50 +0800

    Add ability to store text entered by user and display back to user when requested

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Som User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Som.java
+++ src/main/java/Som.java
@@ -1,15 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Som {
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
         String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println(" Hello! I'm Som\n What can I do for you?");
@@ -17,13 +11,23 @@ public class Som {
 
         Scanner sc = new Scanner(System.in); // Create a new scanner
         String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(line + "\n " + input + "\n" + line);
-            //System.out.println(line);
+        List<String> list = new ArrayList<>(); // Stores user inputs
+        while (!input.equals("bye")) { 
+            if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
+                }
+                System.out.println(line);
+                input = sc.nextLine();
+                continue;
+            }
+            list.add(input);
+            System.out.println(line + "\n added: " + input + "\n" + line);
             input = sc.nextLine();
         }
 
-
+        sc.close(); // Close scanner
         System.out.println(line + "\n Bye. Hope to see you again soon!");
         System.out.println(line);
     }

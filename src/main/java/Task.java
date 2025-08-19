203ef75a6f26b4d1a976026a26bb75393e6b4d14
reven0n <darientan.ssis@gmail.com>
2025-08-19 17:03:50 +0800

    Added the Task Class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return " [" + getStatusIcon() + "] " + description;
+    }
 }

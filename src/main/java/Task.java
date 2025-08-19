ef4e9d35d1e2d4e8aa2ad38d5cd79d86252a2a0f
reven0n <darientan.ssis@gmail.com>
2025-08-20 03:22:49 +0800

    Added ID to each Task for tracking

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,17 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    private int id;
+    static int total = 0;
+
+    public Task() {
+    }
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        total++;
+        this.id = total;
     }
 
     public String getStatusIcon() {
@@ -17,6 +24,10 @@ public class Task {
 
     public void markAsUndone() {
         isDone = false;
+    }
+
+    public int getID() {
+        return this.id;
     }
 
     @Override

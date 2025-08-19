d109d85cdd1961e893e20c3b1518054e51a01c82
reven0n <darientan.ssis@gmail.com>
2025-08-20 03:23:46 +0800

    Added support for tracking three types of tasks: Todo, Deadline and Events

--- src/main/java/Som.java
+++ src/main/java/Som.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Som {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+
+        //starting code
         System.out.println(line);
         System.out.println(" Hello! I'm Som\n What can I do for you?");
         System.out.println(line);
@@ -14,16 +16,17 @@ public class Som {
         List<Task> list = new ArrayList<>(); // Stores user inputs
 
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
+            String task = input.split(" ")[0];
+            if (input.equals("list")) { // Listing out all items
                 System.out.println(line);
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i).toString());
+                for (Task item : list) {
+                    System.out.println(item.getID() + ". " + item.toString());
                 }
 
-            } else if (input.contains("mark")) {
+            } else if (task.contains("mark")) { // Marking and Unmarking items
                 int chosenInt = Character.getNumericValue(input.charAt(input.length() - 1));
                 Task chosenTask = list.get(chosenInt - 1);
-                if (input.contains("un")) {
+                if (task.contains("un")) {
                     chosenTask.markAsUndone();
                     System.out.println(line + "\n OK, I've marked this task as not done yet: \n" + chosenTask.toString());
                 }  else {
@@ -31,9 +34,12 @@ public class Som {
                     System.out.println(line + "\n Nice! I've marked this task as done: \n"+ chosenTask.toString());
                 }
 
-            } else {
-                list.add(new Task(input));
-                System.out.println(line + "\n added: " + input);
+            } else { // Adding items into list
+                Task t = getTask(input, task);
+                list.add(t);
+                System.out.println(line + "\nGot it. I've added this task:");
+                System.out.println(t.toString());
+                System.out.println("Now you have " + Task.total + " tasks in the list\n" + line);
             }
 
             System.out.println(line);
@@ -43,5 +49,41 @@ public class Som {
         sc.close(); // Close scanner
         System.out.println(line + "\n Bye. Hope to see you again soon!");
         System.out.println(line);
+    }
+
+    /**
+     * This method returns a Task based on the user input
+     *
+     * @param input The original user input
+     * @param task  The task requested by the user (todo/deadline/event)
+     * @return      The correct Task type requested by the user
+     */
+    private static Task getTask(String input, String task) {
+        String fullDesc = input.replace(task, "");
+        Task t = new Task();
+        switch (task) {
+            case "todo" -> { // case 1: todo
+                t = new Todo(fullDesc);
+            }
+            case "deadline" -> { // case 2: deadline
+                int byIndex = fullDesc.indexOf("/by ");
+                String desc = fullDesc.substring(0, byIndex).trim();
+                String deadline = fullDesc.substring(byIndex + 4).trim(); // +4 to skip "/by "
+                t = new Deadline(desc, deadline);
+            }
+            case "event" -> { // case 3: event
+                int fromIndex = fullDesc.indexOf("/from ");
+                int toIndex = fullDesc.indexOf("/to ");
+                String from = fullDesc.substring(fromIndex + 6, toIndex).trim(); // +6 to skip "/from "
+                String to = fullDesc.substring(toIndex + 4).trim(); // +6 to skip "/to "
+                String desc = fullDesc.substring(0, fromIndex).trim();
+                t = new Event(desc, from, to);
+            }
+            default -> { // if no special task is assigned
+                t = new Task(input);
+            }
+        }
+
+        return t;
     }
 }
